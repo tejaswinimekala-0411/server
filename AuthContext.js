6d568bbee066d3d04044b265c1ed8384b3d0bad6
tejaswinimekala-0411 <tejumekala04@gmail.com>
2026-04-17 10:45:56 +0530

    Add files via upload

---
+++ AuthContext.js
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (data) => {
+    setUser(data);
+    localStorage.setItem("user", JSON.stringify(data));
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
